258bebed3633a3015d6e45fdb37f8097917dd661
BurhanuddinRaja <mails4burhan@gmail.com>
2023-12-06 18:47:19 +0530

    added instructions in Section Editor

--- src/components/admin/editor/SectionEditor.jsx
+++ src/components/admin/editor/SectionEditor.jsx
@@ -2,12 +2,12 @@
 
 import { Box, IconButton, Grid } from "@mui/material";
 import AddBoxIcon from "@mui/icons-material/AddBox";
+import { useState } from "react";
 
 const Row = ({ children }) => {
   return (
     <>
-      <Box padding={"5px"}>
-        <Box></Box>
+      <Box padding={"5px"} position={"relative"}>
         {children}
       </Box>
     </>
@@ -35,10 +35,29 @@ const Col = () => {
 };
 
 const SectionEditor = () => {
+  const [rows, setRows] = useState([]);
+
+  //   update col - setRows({...rows, cols: cols.map((el) => el.id == id { content = "Update" } return el )})
+  // Swap col - setRows({...rows, cols: })
+
+  let rowsProps = {
+    id: "uuid",
+    count: 1,
+    cols: [
+      {
+        content: "",
+        id: "uuid",
+        count: 1,
+        width: "",
+      },
+    ],
+  };
+
   return (
     <>
       <Box padding={"10px"} backgroundColor={"#cfcfcf"} borderRadius={"7px"}>
         <Row>
+          <Box>{/* Dropdown */}</Box>
           <Grid container justifyContent={"space-evenly"}>
             <Grid item width={"19%"}>
               <Col />
@@ -48,6 +67,7 @@ const SectionEditor = () => {
             </Grid>
           </Grid>
         </Row>
+        {/* Button to Add a new row */}
       </Box>
     </>
   );
